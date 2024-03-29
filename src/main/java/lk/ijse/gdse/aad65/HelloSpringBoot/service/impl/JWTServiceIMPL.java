d29f002b0b7ba52d868441de27af9affd85861a6
ThanuraSilva <thanurasilva1989@gmail.com>
2024-03-29 15:51:40 +0530

    Generate token method

--- src/main/java/lk/ijse/gdse/aad65/HelloSpringBoot/service/impl/JWTServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad65/HelloSpringBoot/service/impl/JWTServiceIMPL.java
@@ -1,11 +1,25 @@
 package lk.ijse.gdse.aad65.HelloSpringBoot.service.impl;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lk.ijse.gdse.aad65.HelloSpringBoot.service.JWTService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
 @Service
 public class JWTServiceIMPL implements JWTService {
+    @Value("${token.key}")
+    private String jwtKey;
+
     @Override
     public String extractUsername(String token) {
         return "";
@@ -19,5 +33,28 @@ public class JWTServiceIMPL implements JWTService {
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
         return false;
+    }
+    // actual process
+    private <T> T extractClaim(String token, Function<Claims,T> claimResolve) {
+        //ToDo:
+        return claimResolve.apply("");
+    }
+
+    private String generateToken(Map<String,Object> extractClaims, UserDetails userDetails){
+        extractClaims.put("role",userDetails.getAuthorities());
+        Date now = new Date();
+        Date expire = new Date(now.getTime() + 1000 * 600);
+
+        String accessToken = Jwts.builder().setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expire)
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+        return accessToken;
+
+    }
+    private Key getSignKey(){
+        byte[] decode = Decoders.BASE64.decode(jwtKey);
+        return Keys.hmacShaKeyFor(decode);
     }
 }
