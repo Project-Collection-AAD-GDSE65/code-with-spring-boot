e23752aeb64178d9775bb44561a2f328aec860b4
ThanuraSilva <thanurasilva1989@gmail.com>
2024-03-29 10:35:39 +0530

    AuthenticationManager enabled

--- src/main/java/lk/ijse/gdse/aad65/HelloSpringBoot/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/aad65/HelloSpringBoot/config/SecurityConfig.java
@@ -1,22 +1,30 @@
 package lk.ijse.gdse.aad65.HelloSpringBoot.config;
 
+import lk.ijse.gdse.aad65.HelloSpringBoot.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserService userService;
     @Bean
    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
        http.csrf(AbstractHttpConfigurer::disable)
@@ -25,14 +33,24 @@ public class SecurityConfig {
                        .anyRequest()
                        .authenticated())
                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-               .authenticationProvider(authenticationProvider()) //Todo:authenticationProvider
+               .authenticationProvider(authenticationProvider()).addFilterBefore() //Todo:authenticationProvider
 
                return http.build();
    }
+         @Bean
+         public PasswordEncoder passwordEncoder() {
+             return new BCryptPasswordEncoder();
+         }
+         @Bean
          public AuthenticationProvider authenticationProvider() {
              DaoAuthenticationProvider dap = new DaoAuthenticationProvider();
-             dap.setUserDetailsService();//Todo:user service
-
+             dap.setUserDetailsService(userService.userDetailsService());
+             dap.setPasswordEncoder(passwordEncoder());
+             return dap;
          }
+
+         @Bean
+         public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+                return configuration.getAuthenticationManager();
    }
 }
